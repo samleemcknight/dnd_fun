264fb963cc48933c0384ef20565827f60656e7b9
samleemcknight <samleemcknight@gmail.com>
2021-05-05 10:26:47 -0600

    routes for dnd races

--- main_app/views.py
+++ main_app/views.py
@@ -4,17 +4,17 @@ import requests
 # Create your views here.
 
 def home(request):
-  races = requests.get('https://www.dnd5eapi.co/api/races')
-  races = races.json()
-  race_info = []
-  for x in races['results']:
-    race = requests.get(f"https://www.dnd5eapi.co/api/races/{x['index']}")
-    race = race.json()
-    race_info.append(race)
-  context = {
-    "race_info": race_info
-  }
-  return render(request, 'index.html', context)
+  # races = requests.get('https://www.dnd5eapi.co/api/races')
+  # races = races.json()
+  # race_info = []
+  # for x in races['results']:
+  #   race = requests.get(f"https://www.dnd5eapi.co/api/races/{x['index']}")
+  #   race = race.json()
+  #   race_info.append(race)
+  # context = {
+  #   "race_info": race_info
+  # }
+  return render(request, 'index.html')
 
 def characters(request):
   return render(request, 'characters.html')
